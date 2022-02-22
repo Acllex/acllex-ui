21fa5f3fc06caa2c736671f703838aff8fd49695
Acllex <w986169823@gmail.com>
2022-02-22 14:26:34 +0800

    初始化vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createWebHashHistory, createRouter} from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 
-createApp(App).mount('#app')
+const history = createWebHashHistory()
+const router = createRouter({
+    history,
+    routes:[
+        {path: '/', component: HelloWorld}
+    ]
+})
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
