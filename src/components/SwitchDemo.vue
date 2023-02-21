245bba5edccb88cdb3b2b4e5121c9f4a557a5d2e
rdpnr_wangguodong <you@example.com>
2023-02-21 11:48:24 +0800

    switch组件功能实现

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
-        <Switch></Switch>
+        <Switch :checked="checked" @submit="checked=$event"></Switch>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
+const checked = ref(false);
 
 </script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,28 +1,42 @@
 <template>
-  <button><span></span></button>
+  <button :class="{checked: props.checked}" @click="setChecked"><span></span></button>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const props = defineProps(['checked'])
+const emit = defineEmits(['submit'])
+const setChecked = ()=>{
+  emit("submit", !props.checked);
+}
+</script>
 
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
-button{
+
+button {
   height: $h;
-  width: $h*2;
+  width: $h * 2;
   border: none;
-  background-color: blue;
+  background-color: rgba(0,0,0,.45);
   border-radius: $h / 2;
   position: relative;
-  span{
+  span {
     position: absolute;
     top: 2px;
     left: 2px;
     height: $h2;
     width: $h2;
     background-color: white;
-    border-radius: $h2 / 2;
+    border-radius: $h2/2;
+    transition: left .5s;
   }
 }
-button:hover > span {
+button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+.checked{
+  background-color: #4096ff;
 }
 </style>
