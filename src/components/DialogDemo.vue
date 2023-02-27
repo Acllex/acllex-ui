4b8b89bf4719153dd6c157fd300f3aa2df3293df
rdpnr_wangguodong <you@example.com>
2023-02-27 17:34:54 +0800

    完善dialog文档

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,11 +1,40 @@
 <template>
   <div class="dialog-demo">
     <h1>基础示例</h1>
-    <Button @click="onclick">Dialog</Button>
-    <Dialog width="600px" v-model:open="isOpen" title="示例1" :closeOnClickOverlay="true" :on-ok="onOk" :on-cancel="onCancel">
-      <p>11111111</p>
-      <p>111222222</p>
-    </Dialog>
+    <div class="mb-1">
+      <Button @click="onclick">Dialog</Button>
+      <Dialog v-model:open="isOpen" title="示例1" >
+        <p>11111111</p>
+        <p>111222222</p>
+      </Dialog>
+    </div>
+    <br>
+    <h1>自定义宽度</h1>
+    <div class="mb-1">
+      <Button @click="()=>{isOpen1 = !isOpen1}">自定义宽度</Button>
+      <Dialog width="800px" v-model:open="isOpen1">
+        <p>11111111</p>
+        <p>111222222</p>
+      </Dialog>
+    </div>
+    <br>
+    <h1>确定和取消的回调</h1>
+    <div class="mb-1">
+      <Button @click="()=>{isOpen2 = !isOpen2}">自定义宽度</Button>
+      <Dialog v-model:open="isOpen2" title="确定和取消的回调" :on-ok="onOk" :on-cancel="onCancel">
+        <p>11111111</p>
+        <p>111222222</p>
+      </Dialog>
+    </div>
+    <br>
+    <h1>禁止点击遮罩层关闭</h1>
+    <div class="mb-1">
+      <Button @click="()=>{isOpen3 = !isOpen3}">自定义宽度</Button>
+      <Dialog v-model:open="isOpen3" :closeOnClickOverlay="false">
+        <p>11111111</p>
+        <p>111222222</p>
+      </Dialog>
+    </div>
   </div>
 
 </template>
@@ -16,6 +45,9 @@ import Dialog from "../lib/Dialog.vue";
 import {ref} from "vue";
 
 const isOpen = ref(false);
+const isOpen1 = ref(false);
+const isOpen2 = ref(false);
+const isOpen3 = ref(false);
 const onclick = () => {
   isOpen.value = !isOpen.value;
 }
@@ -28,7 +60,7 @@ const onCancel = ()=>{
 }
 </script>
 <style scoped lang="scss">
-.button-demo {
+.dialog-demo {
   & > .mb-1 {
     margin-bottom: 4px;
   }
